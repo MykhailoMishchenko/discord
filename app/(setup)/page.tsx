b94c518adfc5bc4973ede776388440cf18d34326
MykhailoMishchenko <mishchenko.mykhailo@gmail.com>
2023-09-24 17:09:38 +0300

    initial modal ui

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import {initialProfile} from "@/lib/initial-profile";
 import {db} from "@/lib/db";
 import {redirect} from "next/navigation";
+import InitialModal from "@/components/modals/initial-modal";
 
 type Props = {
     
@@ -23,9 +24,7 @@ const SetupPage = async (props: Props) => {
     }
 
     return (
-        <div>
-            Create a Server
-        </div>
+        <InitialModal/>
     );
 };
 
